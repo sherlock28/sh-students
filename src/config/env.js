0105db5d26a30233d615dcfe1b9c0ab1d4982008
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-08-13 08:24:41 -0300

    Adding environment vars

---
+++ src/config/env.js
@@ -0,0 +1,6 @@
+export const env = {
+	PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    VERSION: process.env.VERSION ? +process.env.VERSION : 1,
+    APP_DOMAIN: process.env.APP_DOMAIN,
+}
